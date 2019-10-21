8ebd5745849bc20bc7a7d7ea5f72275b493484e6
Zeyu Chen <xinyifly@live.cn>
2019-10-21 18:29:40 +0800

    feat: add Student

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return \App\Student::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return \App\Student::create($request->input());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return \App\Student::findOrFail($id);
+    }
+}
---
+++ tests/Feature/StudentTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class StudentTest extends TestCase
+{
+    public function testIndex()
+    {
+        $student = factory(\App\Student::class)->create();
+        $response = $this->get('/api/students');
+
+        $response->assertStatus(200);
+        $response->assertJsonFragment(['name' => $student->name]);
+    }
+
+    public function testStore()
+    {
+        $student = factory(\App\Student::class)->make();
+        $response = $this->post('/api/students', ['name' => $student->name]);
+
+        $response->assertStatus(201);
+        $response->assertJson(['name' => $student->name]);
+    }
+
+    public function testShow()
+    {
+        $student = factory(\App\Student::class)->create();
+        $response = $this->get("/api/students/{$student->id}");
+
+        $response->assertStatus(200);
+        $response->assertJson(['name' => $student->name]);
+    }
+}

225ed96c6eddcd89e85c2a921b197a4ac0ea3fec
Zeyu Chen <xinyifly@live.cn>
2019-10-22 01:51:56 +0800

    fix: Courses, Students names add uniques

---
+++ database/migrations/2019_10_21_172858_add_unique_to_courses_and_students.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUniqueToCoursesAndStudents extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('courses', function (Blueprint $table) {
+            $table->unique('name');
+        });
+        Schema::table('students', function (Blueprint $table) {
+            $table->unique('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('students', function (Blueprint $table) {
+            $table->dropUnique('name');
+        });
+        Schema::table('courses', function (Blueprint $table) {
+            $table->dropUnique('name');
+        });
+    }
+}
